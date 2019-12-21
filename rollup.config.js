33c2dca43c0e496c81b3bf0b7a6d26e50a0bfbc9
Tootoot222 <Tootoot222@ttt222.org>
2019-12-20 22:49:57 -0500

    Use rollup to create proper bundle module

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from 'rollup-plugin-babel';
+import typescript from 'typescript';
+import rollupTypescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    babel(),
+    rollupTypescript({
+      typescript,
+    }),
+  ],
+};
