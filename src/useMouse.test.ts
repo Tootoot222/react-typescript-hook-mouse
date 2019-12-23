4074510f36afd407a3db83ff703511547520b3c2
Tootoot222 <Tootoot222@ttt222.org>
2019-12-22 23:16:16 -0500

    eslint remove blank line

--- src/useMouse.test.ts
+++ src/useMouse.test.ts
@@ -20,7 +20,6 @@ function isMouseEvent(event: string): event is EventListenerMouseEvent {
 }
 
 describe('useMouse hook', () => {
-
   let events: {
     [key in EventListenerMouseEvent]: MouseEventHandler[];
   };
